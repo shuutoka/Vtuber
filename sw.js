07a82fe4eb8e135e0eb8349a030e32d52929dfd3
shuutoka <80158458+shuutoka@users.noreply.github.com>
2025-02-07 20:05:26 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker installé.");
+    event.waitUntil(
+        caches.open("v1").then(cache => {
+            return cache.addAll([
+                "/",
+                "/pages/selection",
+                "/manifest.json",
+                "/loading.css",
+                "/script.js",
+                "/icons/web-app-manifest-192x192.png",
+                "/icons/web-app-manifest-512x512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
