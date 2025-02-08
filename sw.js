190b3a57117e016d4abc5c20a5d6de2904d4ff78
shuutoka <80158458+shuutoka@users.noreply.github.com>
2025-02-08 18:26:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -29,11 +29,3 @@ self.addEventListener("fetch", event => {
     );
 });
 
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
